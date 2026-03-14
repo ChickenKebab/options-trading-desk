cda87b961b71df6c018db6d51a604ef743d2527f
ChickenKebab <matty.kiraly.jr@outlook.com>
2026-03-14 15:07:08 +0000

    implements Black-Scholes pricing engine

---
+++ pricing/black_scholes.py
@@ -0,0 +1,53 @@
+import numpy as np
+from scipy.stats import norm
+
+
+class EuropeanOption:
+
+    def __init__(self, S, K, r, sigma, T, option_type):
+        if option_type not in ['call', 'put']:
+            raise ValueError("option_type must be 'call' or 'put'")  
+        self.S = S
+        self.K = K
+        self.r = r
+        self.sigma = sigma
+        self.T = T
+        self.option_type = option_type.lower() 
+        self.d1 = self._d1()
+        self.d2 = self._d2()
+
+    
+    def _d1(self):
+        d1 = (np.log(self.S/self.K) + (self.r + 0.5*self.sigma**2)*self.T)/(self.sigma*np.sqrt(self.T))
+        return d1
+    
+
+    def _d2(self):
+        d2 = self.d1 - self.sigma*np.sqrt(self.T)
+        return d2
+    
+    def price(self):
+
+        
+        
+        
+
+        if self.option_type == 'call':
+            call_price = self.S*(norm.cdf(self.d1))-self.K*np.exp(-self.r*self.T)*(norm.cdf(self.d2))
+            return call_price
+        
+        else:
+            put_price = self.K*np.exp(-self.r*self.T)*(norm.cdf(-self.d2))-self.S*(norm.cdf(-self.d1))
+            return put_price
+        
+
+## TEST
+
+if __name__ == "__main__":
+    call = EuropeanOption(S=150, K=150, r=0.05, sigma=0.25, T=1, option_type='call')
+    print(f"d1: {call.d1:.4f}")
+    print(f"d2: {call.d2:.4f}")
+    print(f"Call Price: ${call.price():.4f}")
+    
+    put = EuropeanOption(S=150, K=150, r=0.05, sigma=0.25, T=1, option_type='put')
+    print(f"Put Price: $ {put.price():.4f}")
